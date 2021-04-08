22ec9205d17d4c7680018b63f9638541ce08051e
YoonMin <80492096+YoonminBr@users.noreply.github.com>
2021-04-08 17:28:17 -0300

    Update and rename server.js to index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const Twit = require('twit');
+
+const config = {
+  consumer_key:         process.env.BOT_CONSUMER_KEY,
+  consumer_secret:      process.env.BOT_CONSUMER_SECRET,
+  access_token_key:     process.env.BOT_ACESS_TOKEN,
+  access_token_secret:  process.env.BOT_ACESS_TOKEN_SECRET,
+}
+ */
+const words = 'yoonmin, #yoonmin';
+
+/*
+  */
+const screenName = 'BotYMBr';
+
+
+const twit = new Twit(config);
+const stream = twit.stream('statuses/filter', { track: words.split(',').map(w => w.trim()) });
+
+console.log('Bot is starting!');
+try {
+  stream.on('tweet', tweet => {
+    if (tweet.user.screen_name === screenName) return;
+
+    if (!tweet.retweeted_status) {
+      twit.post('statuses/retweet/:id', { id: tweet.id_str }, (err, data) => {
+        if (err || !data.text) {
+          return console.log(`Error retweeting user "@${tweet.user.screen_name}"`);
+        }
+        console.log(`Retweeted user "@${tweet.user.screen_name}":\n"${tweet.text}"\n`);
+      });
+    }
+  });
+} catch (_) {
+  console.log('An error ocurred.');
+  process.exit();
+}
