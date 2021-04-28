b00c65dec89c96d454a13fc3b62148f395234363
unknown <ficsyoonminbr@gmail.com>
2021-04-28 00:13:20 -0300

    botymbr

--- twitter-bot-template-master/index.js
+++ twitter-bot-template-master/index.js
@@ -8,6 +8,7 @@ const config = {
   consumer_secret: 'WqseUMdDsXq4cxQOFFJHqN16bBj4Z8IYJ817PaJwC3TTL0HFAu',
   access_token: '1333257545532706817-v4BiYVhZCuuiCeizcMEwOfWSgGHB6H',
   access_token_secret: 'BPG0anMqxZFRlZsfzuCiJMyVjrRmBaSAtrKebGeJ4mF9y',
+
 };
 
 /*
@@ -25,6 +26,10 @@ const words = 'yoonmin,#yoonmin';
  * - Example: if your account '@' is '@twitter_bot', you should write only 'twitter_bot'
  */
 const screenName = 'BotYMBr';
+
+/* iniciar funcao bot 
+*/
+function BotInit(){
 
 /*
  * If you are a not a programmer, avoid changing anything on the next lines
@@ -51,3 +56,8 @@ try {
   console.log('An error ocurred.');
   process.exit();
 }
+}
+setInterval(Twit, 30 * 60 * 1000);
+
+/* iniciar o bot Bot */
+BotInit();
